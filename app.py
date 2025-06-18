fe340283949182565739c735780c2ecf127f3e24
MMNASH10 <morganmaenash@gmail.com>
2025-06-18 12:41:01 -0400

    testing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 # import pandas as pd
 # import geopandas as gpd
-# from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download
 # import pydeck as pdk
 # import folium
 # from streamlit_folium import st_folium
